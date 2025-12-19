a452f79ee00130d52fcfdb1fac2d06fcac833406
Victor <Vboiko891@gmail.com>
2025-12-19 12:16:09 +0300

    Сохранение данных  в CSV

---
+++ src/main/java/otc/FileUtils.java
@@ -0,0 +1,29 @@
+package otc;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class FileUtils {
+
+    public static void saveToCsv(List<Map<String, String>> data, String filePath) {
+
+        try (FileWriter writer = new FileWriter(filePath)) {
+
+            // Заголовки
+            writer.write("name,price\n");
+
+            // Данные
+            for (Map<String, String> item : data) {
+                writer.write(
+                        item.get("name") + "," +
+                                item.get("price") + "\n"
+                );
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("Ошибка записи CSV файла", e);
+        }
+    }
+}
